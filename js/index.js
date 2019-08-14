c2a860b072093272b62e71481d1bea968be5f2b6
leahchow <ctzhouleya@gmail.com>
2019-08-14 09:09:21 -0400

    second-commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-d3.json(`data/data.json`).then(dataset=>{
+d3.json(`data/trandata.json`).then(dataset=>{
   console.log(dataset)
-  //YU Yellow
-  //BD Green
+  //yu Yellow
+  //bd Green
   //SHP purple
   //SRT blue
 
@@ -9,50 +9,70 @@ d3.json(`data/data.json`).then(dataset=>{
   console.log(dataset[45].minDelay)
   
   const newData = dataset.filter(arr=>arr.minDelay!==0)
-  console.log(newData[29])
+   console.log(newData)
+   console.log(newData[12].timeInHrs)
 
   setWidth=1600, 
   setHeight= 2000;
   let quantities = newData.length;
-  
 
 
-
-
-d3.select(`#time-line`)
-.selectAll(`line`)
-.data(newData)
-.enter()
-.append(`line`)
-.attr(`x1`,0)
-.attr(`y1`,(num,i)=>i*10)
-.attr(`x2`,(num,i)=>newData[i].minDelay*10)
-.attr(`y2`,(num,i)=>i*10)
-.attr(`stroke-width`,0.7)
-.attr(`stroke`,(num,i)=>newData[i].color)
-
-
-  // for(i=0;i<dataset.lenght;i++){
-  //   if(dataset[i].Line=='BD'){
-  //     d3.select(`svg`)
-  //     .append(`line`)
-  //     .attr(`color`, green)
-  //     .attr(`x1`,)
-  //   }
-  // }
    
-  // setWidth=400, 
-  // setHeight= 100;
 
-  // const svg = d3.select(`section`)
-  // .append(`svg`)
-  // .attr(`width`,setWidth)
-  // .attr(`height`,setHeight)
-  
-   
-  // let xscale = d3.scaleLinear()
-  //                .domain([0,i=>dataset[i].Time])
-  //                .range([0,])
-                
 
+ d3.select(`#time-line`)
+ .selectAll(`line`)
+ .data(newData)
+ .enter()
+ .append(`line`)
+ .attr(`x1`,(num,i)=>newData[i].timeInHrs*100)
+ .attr(`y1`,800)
+ .attr(`x2`,(num,i)=>newData[i].timeInHrs*100) // delay time in hour
+ .attr(`y2`,(num,i)=>800-newData[i].minDelay*5) //how long it delay in mins
+ .attr(`stroke-width`,1.5)
+ .attr(`stroke`,(num,i)=>newData[i].color)
+
+
+
+
+
+
+
+/**
+ * ? how to draw the axis base on data?
+ */
+
+// var width = 400, height = 100;
+
+// var data = [10, 15, 20, 25, 30];
+// var svg = d3.select("body")
+//             .append("svg")
+//             .attr("width", width)
+//             .attr("height", height);
+
+// var scale = d3.scaleLinear()
+//               .domain([d3.min(data), d3.max(data)])
+//               .range([height/2, 0]);
+
+// var y_axis = d3.axisLeft()
+//               .scale(scale);
+
+// svg.append("g")
+//    .attr("transform", "translate(50, 10)")
+//    .call(y_axis);
+ 
+
+
+    
+ // drawing data in circles
+  //  d3.select(`#time-line`)
+  //  .selectAll(`circle`)
+  //  .data(newData)
+  //  .enter()
+  //  .append(`circle`)
+  //  .attr(`cx`,(num,i)=>newData[i].timeInHrs*1000)
+  //  .attr(`cy`,900)
+  //  .attr(`r`,(num,i)=>newData[i].minDelay*10)
+  //  .attr(`fill`,(num,i)=>newData[i].color)
+  //  .attr(`stroke`,`black`)
 })
